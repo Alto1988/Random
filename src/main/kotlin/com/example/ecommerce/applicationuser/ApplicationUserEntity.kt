9fd5560949a2dbc2707cf5bbe25093f2276fe595
Jacob Pagan <jake2415@gmail.com>
2022-12-06 21:51:55 -0600

    WIP still not done with the initial setup

--- src/main/kotlin/com/example/ecommerce/applicationuser/ApplicationUserEntity.kt
+++ src/main/kotlin/com/example/ecommerce/applicationuser/ApplicationUserEntity.kt
@@ -1,14 +1,14 @@
-package com.example.ecommerce.applicationuser
-
-import sun.jvm.hotspot.gc.shared.Generation
-import javax.persistence.*
-
-@Entity
-@Table(name="users")
-class ApplicationUserEntity(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long,
-
-) {
-}
+//package com.example.ecommerce.applicationuser
+//
+//import sun.jvm.hotspot.gc.shared.Generation
+//import javax.persistence.*
+//
+//@Entity
+//@Table(name="users")
+//class ApplicationUserEntity(
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    var id: Long,
+//
+//) {
+//}
--- src/main/kotlin/com/example/ecommerce/config/CorsConfig.kt
+++ src/main/kotlin/com/example/ecommerce/config/CorsConfig.kt
@@ -1,20 +1,31 @@
 package com.example.ecommerce.config
 
+
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @Configuration
 class CorsConfig {
+
+
     @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource{
-        var configuration = CorsConfiguration()
-            .apply {
-                allowedOrigins = listOf("http://localhost:4200")
-                allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                allowedHeaders = listOf("*")
-            }
-        return configuration as CorsConfigurationSource
+    fun corsFilter(): CorsFilter {
+        return CorsFilter(corsConfigurationSource())
+    }
+
+    @Bean
+    fun corsConfigurationSource(): UrlBasedCorsConfigurationSource {
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("http://localhost:8080")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        return source
     }
 }
